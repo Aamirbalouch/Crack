d9622fae3118be2544353ddc7c638086981dc360
Aamirbalouch <83411055+Aamirbalouch@users.noreply.github.com>
2021-10-22 12:09:17 +0500

    Create Crack.py

---
+++ Crack.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python2
+import re,os,requests
+import time
+from os import system
+from platform import platform
+from time import sleep
+import sys
+sys.platform
+'wind32', 'wind64', 'linux', '32bit', '64bit', 'android'
+import os
+xoo = platform()[0], platform()[1],  platform()[2], platform()[3], platform()[4], platform()[5], platform()[6]
+
+if xoo == ('W', 'i', 'n', 'd', 'o', 'w', 's'):
+    abm = 'cls'
+    dr = '\\'
+else:
+    abm = 'clear'
+    dr = '/'
+
+os.system(akb)
+time.sleep(1)
+print("loading modules...")
+time.sleep(0.05)
+os.system(akb)
+print("lOading modules...")
+time.sleep(0.05)
+os.system(akb)
+print("loAding modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loaDing modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loadIng modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loadiNg modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loadinG modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loading modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loading mOdules...")
+time.sleep(0.1)
+os.system(akb)
+print("loading moDules...")
+time.sleep(0.1)
+os.system(akb)
+print("loading modUles...")
+time.sleep(0.1)
+os.system(akb)
+print("loading moduLes...")
+time.sleep(0.1)
+os.system(akb)
+print("loading modulEs...")
+time.sleep(0.1)
+os.system(akb)
+print("loading moduleS...")
+time.sleep(0.1)
+os.system(akb)
+print("loading modules...")
+time.sleep(0.1)
+os.system(akb)
+print("Loading modules...")
+time.sleep(0.1)
+os.system(akb)
+print("lOading modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loAding modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loaDing modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loadIng modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loadiNg modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loadinG modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loading Modules...")
+time.sleep(0.1)
+os.system(akb)
+print("loading mOdules...")
+time.sleep(0.1)
+os.system(akb)
+print("loading moDules...")
+time.sleep(0.1)
+os.system(akb)
+print("loading modUles...")
+time.sleep(0.1)
+os.system(akb)
+print("loading moduLes...")
+time.sleep(0.1)
+os.system(akb)
+print("loading modulEs...")
+time.sleep(0.1)
+os.system(akb)
+print("loading moduleS...")
+time.sleep(0.1)
+os.system(akb)
+print("loading modules...")
+time.sleep(0.1)
+os.system(akb)
+
+def banner():
+    print("""
+               __       __   ___  _______   
+     /""\     |/"| /  ")|   _  "\  
+    /    \    (: |/   / (. |_)  :) 
+   /' /\  \   |    __/  |:     \/  
+  //  __'  \  (// _  \  (|  _  \\  
+ /   /  \\  \ |: | \  \ |: |_)  :) 
+(___/    \___)(__|  \__)(_______/  
+                                   '                               
+------------------------------------------------- 
+(~) Author  : Tech Akb
+(~) Github  : https://github.com/Tech-AKB
+(~) Fb Page : https://facebook.com/TechAKB
+-------------------------------------------------   
+""")
+
+def main():
+    os.system("clear")
+    banner()
+    print("[1] Install Fbc Fb Number Cloing  (no login)");time.sleep(0.05)
+    print("[2] Install Fbc Tool Extracting    (Simple)");time.sleep(0.05)
+    print("[3] Install Fbc Tool Extracting      (Vip)");time.sleep(0.05)
+    print("[4] Tool Exit");time.sleep(0.05)
+    print("-------------------------------------------------");time.sleep(0.05)
+    m()
+def m():
+    user_option = raw_input("\n[!] Select an valid option : ")
+    if user_option =="1": 
+        os.system("cd num && python2 num.py")
+    elif user_option =="2":
+        os.system("cd old_main && python2 main.py")
+    elif user_option =="3":
+        os.system("cd fbc_main && python2 install.py")
+    elif user_option =="4":
+        print("")
+        print("Tool Logout Successfull").center(50)
+        time.sleep(1)
+        os.system("exit")
+    else:
+        print("")
+        print("Please select an correct option").center(50)
+        print("")
+        time.sleep(1)
+        main()
+
+if __name__ == '__main__':
+    main()
